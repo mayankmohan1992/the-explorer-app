16771e44163281919ad7cad91ebbb250bf4c73e1
Mayank Mohan <mayankmohan@Mayanks-MacBook-Air-2.local>
2026-06-19 23:24:04 +0530

    Fix compilation error by adding missing border import to MediaTab

--- app/src/main/java/com/explorer/app/ui/media/MediaTab.kt
+++ app/src/main/java/com/explorer/app/ui/media/MediaTab.kt
@@ -9,6 +9,7 @@ import android.widget.FrameLayout
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.layout.*
